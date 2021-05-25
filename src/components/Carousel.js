d3dc1ee899640dc283ee9fdf8da2febf11ba09d0
petersenea <Petersenea12@gmail.com>
2021-05-25 12:16:33 -0500

    fixed styling

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -24,7 +24,22 @@ class Carousel extends React.Component {
         // GET from our backend whether this image is favorited
         // input: image date (YYYY-MM-DD)
         // output: image or null
-        this.setState({ favoriteButton: "Favorite" })
+        fetch("https://apod-backend.herokuapp.com/images/favorited", {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Content-Type': 'application/json',
+                'date': '2021-05-10'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            // body: JSON.stringify({date: "2021-05-10"}) // body data type must match "Content-Type" header
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data != null) {
+                    this.setState({ favoriteButton: "Favorite" })
+                }
+                console.log('isFavorite()', data);
+            });
     }
 
     favorite() {
@@ -33,12 +48,35 @@ class Carousel extends React.Component {
             // POST to our backend to favorite this image
             // input: image object
             // output: 200 success
+            fetch("https://apod-backend.herokuapp.com/images/favorites", {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(this.props.images[this.state.currentIndex]) // body data type must match "Content-Type" header
+            })
+                .then(response => {
+                    console.log("Favoriting", this.props.images[this.state.currentIndex].date, "with status", response.status)
+                })
         }
         else {
             this.setState({ favoriteButton: "Favorite" })
-            // POST to our backend to unfavorite this image
+            // DELETE to our backend to unfavorite this image
             // input: image date (YYYY-MM-DD)
             // output: 200 success
+            fetch("https://apod-backend.herokuapp.com/images/favorited", {
+                method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify({ date: this.props.images[this.state.currentIndex].date }) // body data type must match "Content-Type" header
+            })
+                .then(response => {
+                    console.log("Unfavoriting", this.props.images[this.state.currentIndex].date, "with status", response.status)
+                })
+
         }
     }
 
@@ -57,10 +95,9 @@ class Carousel extends React.Component {
 
         let newIndex = this.state.currentIndex + 1;
 
-        if(newIndex > this.props.images.length -1){
+        if (newIndex > this.props.images.length - 1) {
             newIndex = 0
         }
-        // let newIndex = (this.state.currentIndex + 1) % this.props.images.length;
         this.setState({ currentIndex: newIndex });
     }
 
@@ -72,17 +109,36 @@ class Carousel extends React.Component {
                     <div>
                     </div>
                     :
-                    <div>
+                    <div className="carousel-container">
+
                         <div className="carousel">
-                            <button onClick={this.goBack}>Back</button>
-                            <img loading="eager" alt={this.props.images[this.state.currentIndex].date} src={this.props.images[this.state.currentIndex].url}></img>
-                            <button onClick={this.goNext}>Next</button>
+                            <button onClick={this.goBack} className="fas fa-chevron-left"></button>
+                            <figure>
+                                {this.props.images[this.state.currentIndex].media_type === "video" ?
+                                    <iframe src={this.props.images[this.state.currentIndex].url} title={this.props.images[this.state.currentIndex].date}></iframe>
+                                    :
+                                    <img loading="eager" alt={this.props.images[this.state.currentIndex].date} src={this.props.images[this.state.currentIndex].url}></img>
+                                }
+                                <figcaption>
+                                    <div className="image-sub">
+                                        <p className="image-count">{this.state.currentIndex + 1} / {this.props.images.length}</p>
+                                        <button onClick={this.favorite}>
+                                            {this.state.favoriteButton === "Favorite" ?
+                                                <i class="far fa-star"></i>
+                                                :
+                                                <i class="fas fa-star"></i>}
+                                        </button>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                            <button onClick={this.goNext}><i className="fas fa-chevron-right"></i></button>
+
+
                         </div>
-                        
-                        <button onClick={this.favorite}>{this.state.favoriteButton}</button>
+
+
 
                         <div className="image-info">
-                            <p className="image-count">{this.state.currentIndex+1} / {this.props.images.length}</p>
                             <p className="image-title">{this.props.images[this.state.currentIndex].title}</p>
                             <p className="image-date">{this.props.images[this.state.currentIndex].date}</p>
                             <p className="image-description">{this.props.images[this.state.currentIndex].explanation}</p>
